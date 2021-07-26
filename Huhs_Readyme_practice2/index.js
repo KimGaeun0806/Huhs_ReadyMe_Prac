a51c55bec3a94551d96a283f5d103e1d8b390199
yellowOctopus <greencloud11@naver.com>
2021-07-26 21:11:26 +0900

    2021.07.26

--- Huhs_Readyme_practice2/index.js
+++ Huhs_Readyme_practice2/index.js
@@ -1,9 +1,5 @@
-const img = document.querySelector("#img");
 const button = document.querySelector('#btn_submit');
 button.addEventListener("click", submit);
-const userName = document.querySelector('p_user_id');
-
-
 
 function submit (){
     let user = {
@@ -21,14 +17,48 @@ function submit (){
     .then(res => res.json())
     .then(data => {
         console.log(data);
-        img.src = data.profile_url;
-        userName.textContent = ID; 
+
+        const profileImg = document.createElement('img');
+        profileImg.src = data.profile_url;
+        document.querySelector('#img').innerHTML='';
+        const userImg = document.querySelector('#img');
+        userImg.append(profileImg);
+
+        const profileId = document.createElement('p');
+        profileId.textContent = data.user_id;
+        document.querySelector('#p_user_id').innerHTML='';
+        const userId = document.querySelector('#p_user_id');
+        userId.append(profileId); 
+
+        const profileName = document.createElement('p');
+        profileName.textContent = data.username;
+        document.querySelector('#p_username').innerHTML='';
+        const userName = document.querySelector('#p_username');
+        userName.append(profileName);
+
+        const profileBio = document.createElement('p');
+        profileBio.textContent = data.bio;
+        document.querySelector('#p_bio').innerHTML='';
+        const userBio = document.querySelector('#p_bio');
+        userBio.append(profileBio);
+
+        const profileAccess = document.createElement('p');
+        profileBio.textContent = data.access_token;
+        document.querySelector('#p_access_token').innerHTML='';
+        const userAccess = document.querySelector('#p_access_token');
+        userAccess.append(profileAccess);
+
+        button.addEventListener("click", newPage);
+
+        function newPage(){
+            window.location.href="netflix.html";
+        }
 
 
-        
 
-       
-
-    
     }).catch(error => {console.log(error)})
+}
+
+function netflix(){
+    
 }
